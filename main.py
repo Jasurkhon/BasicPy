3ae19e8415c67fec31fcca8edcc63c04d76a10c7
Jasurkhon <jasur.uzb10@gmail.com>
2022-01-12 13:05:16 +0500

    intro to python

---
+++ main.py
@@ -0,0 +1,49 @@
+'''
+Begining to python journey)
+
+'''
+# first_name = input("Print your First name, Please: ")
+# print("Hello", first_name)
+
+# first_name = input("Input your First Name: ")
+# last_name = input("Input your Last Name: ")
+# print("Hello", first_name, last_name)
+
+# print("\n What do you call a bear with no teeth?\n A gummy bear!")
+
+# num1 = int(input("Input num1: "))
+# num2 = int(input("Input num2: "))
+# Total = num1 + num2
+# print(num1, "+", num2, " = ", Total)
+
+# num1 = int(input("Input num1: "))
+# num2 = int(input("Input num2: "))
+# num3 = int(input("Input num3: "))
+# Total = (num1 + num2)*num3
+# print("(num1+num2)*num3", "=", Total)
+
+# name = input("Input your name: ")
+# age = int(input("Input your age: "))
+# print(name, "Next birthday you will be", age+1)
+
+# Total_price = int(input("What is the Total price for dinner: "))
+# num_dinner = int(input("What is the number of dinners: "))
+# answer = Total_price//num_dinner
+# print("Each dinner can pay with: ", answer)
+
+# day = int(input("Enter the number of days: "))
+# hour = day*24
+# minute = day*60*24
+# second = day*24*3600
+# print(day, " = ", "Hours:",hour, ",Minutes:", minute, ",Seconds:", second)
+
+# pound = 2.204
+# weight = int(input("Enter weight in kg: "))
+# print("Given weight in pounds:", weight*pound)
+
+# large = int(input("Enter the number over 100: "))
+# small = int(input("Enter the number less than 10: "))
+# total = large//small
+# print(small, "goes into", large, ",", total, "times" )
+
+
