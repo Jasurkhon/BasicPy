a55c6046404c1e291ee4c1f56009394a10b9293d
Jasurkhon <jasur.uzb10@gmail.com>
2022-01-12 13:11:58 +0500

    if-else challenges

---
+++ app.py
@@ -0,0 +1,65 @@
+# if else challenges
+
+# num1 = int(input("Enter num1: "))
+# num2 = int(input("Enter num2: "))
+# if num1 > num2:
+#     print(num2, num1)
+# else:
+#     print(num1, num2)
+
+# num = int(input("Enter the number under 20: "))
+# if num == 20 or num > 20:
+#     print("Too high!")
+# else:
+#     print("Thank you!")
+
+# num = int(input("Enter the num between 10 and 20: "))
+# if num >= 10 and num <= 20:
+#     print("Thank you!")
+# else:
+#     print("Incorrect input!")
+
+# color = input("Enter your favourite color: ")
+# if color == "red" or color == "RED" or color == "Red":
+#     print("I like red too!")
+# else:
+#     print("I don't like", color, "I prefer red")
+
+# raining = input("Is it raining (yes or no)?: ")
+# raining = str.lower(raining)
+# if raining == "yes":
+#     windy = input("Is it windy(yes or no ?: ")    
+#     if windy == "yes":
+#         print("It is too windy for an umbrella!")
+#     else:
+#         print("Take an umbrella!")
+# else:
+#     print("Enjoy your day")  
+
+# age = int(input("Enter your age: "))
+# if age == 18 or age > 18:
+#     print("You can vote!")
+# elif age == 17:
+#     print("You can learn to drive")
+# elif age == 16:
+#     print("You can buy a lottery ticket")
+# elif age < 16:
+#     print("You can go Trick")
+
+# num = int(input("Enter the number: "))
+# if num < 10:
+#     print("Too low!")
+# elif num >= 10 and num <= 20:
+#     print("Correct!")
+# else:
+#     print("Too high!")
+
+# num = int(input("Enter 1, 2 or 3: "))
+# if num == 1:
+#     print("Thank you")
+# elif num == 2:
+#     print("Well done")
+# elif num == 3:
+#     print("Correct")
+# else:
+#     print("Error message! Try again")
