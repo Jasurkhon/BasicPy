5a75077bbfeace7f7270a57b70dc828d785bff9a
Jasurkhon <jasur.uzb10@gmail.com>
2022-01-13 11:50:26 +0500

    string challenges

---
+++ string.py
@@ -0,0 +1,48 @@
+# strings
+
+# name = "  John  "
+# text = "./helloworld.......`@#$%^"
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.capitalize())
+# print(name.title())
+# print(name.strip(""))
+# print(text.strip("./`@#$%^"))
+
+# name = input("Enter your name: ")
+# print("The length of your name: ", len(name))
+
+# firstname = input("Enter your firstname: ")
+# secondname = input("Enter your secondname: ")
+# wholename = firstname + " " + secondname
+# print(wholename, len(wholename))
+
+# firstname = input("Enter your firstname in lower: ")
+# secondname = input("Enter your secondname in lower: ")
+# wholename = firstname + " " + secondname
+# print(wholename.title())
+
+# text = input("Enter the 1st line of a nursery rhyme: ")
+# print("The length of text: ", len(text))
+# start = int(input("Enter starting number: "))
+# end = int(input("Enter ending number: "))
+# print(text[start:end])
+
+# text = input("Enter any word: ")
+# print(text.upper())
+
+# firstname = input("Enter your firstname: ")
+# if len(firstname) < 5:
+#     lastname = input("Enter your lastname: ")
+#     print((firstname + lastname).upper())
+# elif len(firstname) >= 5:
+#     print(firstname.lower())
+
+# word = input("Enter a word: ")
+# if word[0]!="a" and word[0]!="e" and word[0]!="i" and word[0]!="o" and word[0]!="u":
+#     consonant = word[1:len(word)] + word[0] + "ay"
+#     print(consonant.lower())
+# else:
+#     vowel = word[0:len(word)] + "way"
+#     print(vowel.lower())
