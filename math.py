e4c52cd16bf572a3bbfe08e653b9096f88c44def
Jasurkhon <jasur.uzb10@gmail.com>
2022-01-13 12:57:34 +0500

    math challenges

---
+++ math.py
@@ -0,0 +1,45 @@
+# math challenges
+# before using math fucntions we need to import math library at the start of a program
+# print(round(num,2))
+# math.sqrt(num)
+# num = float(input("Enter number:"))
+# math.pi
+# x//y
+# x%y
+# ** power(5**3 = 5^3)
+
+# import math
+
+# num = float(input("Enter the number with lot of decimal places: "))
+# print(num*2)
+
+# num = float(input("Enter the number with lot of decimal places: "))
+# print(round(num*2,2))
+
+# num = int(input("Enter the number over 500: "))
+# print(round(math.sqrt(num),2))
+
+# radius = int(input("Enter the radius of a triangle: "))
+# Area = round((math.pi*radius**2),1)
+# print(Area)
+
+# radius = int(input("Enter radius of a cylinder: "))
+# height = int(input("Enter the height of a cylinder: "))
+# Volume = math.pi*radius**2*height
+# print(round(Volume,3))
+
+# num1 = int(input("Enter num1: "))
+# num2 = int(input("Enter num2: "))
+# print(num1, "divided", num2, "is", num1//num2, "with", num1%num2, "remaining")
+
+# print("\n1)Square\n2)Triangle")
+# num = int(input("Enter a number: "))
+# if num == 1:
+#     side = int(input("Enter the side of a square: "))
+#     print("Area of a square is ", side**2)
+# elif num == 2:
+#     base = int(input("Enter the base of a triangle: "))
+#     height = int(input("Enter the height of a triangle: "))
+#     print("Area of a triangle is ", (base*height)/2)
+# else:
+#     print("Error input! Please choose only 1 and 2 options!!!")
