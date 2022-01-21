b90e4c42c9ab2800b937eadeac0080e2a0ad3d4b
Jasurkhon <jasur.uzb10@gmail.com>
2022-01-22 01:41:47 +0500

    random challenges

---
+++ random_challenge.py
@@ -0,0 +1,84 @@
+#  random challenges
+
+import random
+from turtle import color
+
+# num = random.random()
+# num = random.randint(0,9)
+# num = random.randrange(0,10,2)
+# colour = random.choice(["red", "green", "yellow"])
+# print(num)
+# print(colour)
+
+# num = random.randint(1,100)
+# print(num)
+
+# fruit = random.choice(["apple", "banana", "orange", "avocado", "cherry"])
+# print(fruit)
+
+# Computer_choice = random.choice(["h", "t"])
+# User_choice = input("Make a choice: ")
+# if Computer_choice == User_choice:
+#     print("You win")
+# else:
+#     print("Bad luck")
+# print("Computer selected", Computer_choice)
+
+# num = random.randint(1,5)
+# pick = int(input("Pick a number: "))
+# if pick == num:
+#     print("Well done")
+# else:
+#     if pick < num:
+#         print("Too low")
+#     elif pick > num:
+#         print("Too high")
+#     pick = int(input("Try second pick: "))
+#     if pick == num:
+#         print("Correct")
+#     else:
+#         print("You lose")
+
+# pick = random.randint(1, 10)
+# num = int(input("Enter a number: "))
+# while num !=pick:
+#     num = int(input("Enter a number: "))
+
+# pick = random.randint(1, 10)
+# num = int(input("Enter a number: "))
+# while num !=pick:
+#     if num > pick:
+#         print("Too high")
+#     else:
+#         print("Too low")
+    
+# score = 0
+# for i in range (1, 6):
+#     num1 = random.randint(1, 50)
+#     num2 = random.randint(1,50)
+#     sum = num1+num2
+#     print(num1, "+", num2, "=")
+#     answer = int(input("Enter your answer: "))
+#     if sum == answer:
+#         score = score+1
+# print("You have got", score, "correct answers out of five")
+    
+# color = random.choice(["red", "blue", "yellow", "green", "purple"])
+# print("Pick one color out of options: red , blue, yellow, green, purple")
+# againChoice = True
+# while againChoice == True:
+#     pick = input("Enter a colour: ") 
+#     if color == pick.lower():
+#         print("Well done")
+#         againChoice = False
+#     else:
+#         if color == "red":
+#             print("You are probably feeling", pick, "right now")
+#         elif color == "blue":
+#             print("You are probably feeling", pick, "right now")
+#         elif color == "yellow":
+#             print("You are probably feeling", pick, "right now")
+#         elif color == "green":
+#             print("You are probably feeling", pick, "right now")
+#         elif color == "purple":
+#             print("You are probably feeling", pick, "right now")
