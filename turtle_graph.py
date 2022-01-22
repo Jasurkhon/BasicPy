d9e452d8586ab35f8ccaf1f1db99e1989012e437
Jasurkhon <jasur.uzb10@gmail.com>
2022-01-22 21:26:21 +0500

    turtle graph challenges

---
+++ turtle_graph.py
@@ -0,0 +1,144 @@
+from random import randrange
+import turtle
+import random
+scr = turtle.Screen()
+scr.bgcolor("purple")
+turtle.shape("turtle")
+
+# turtle.penup() turtle doesn't leave a trail
+# turtle.pendown() turtle will leave a trail by default
+# turtle.pensize(3) changes the pen size 
+# turtle.hideturtle() hides turtle from the screen
+# turtle.begin_fill() it knows to fill the shape drawing(before code start)
+# turtle.end_fill() stop filling in the shape(in the end of code)
+# turtle.color("red", "yellow") defines the colour filling in the shape
+# turtle.exitonclick() close when user clicks the window
+
+# for i in range(0,10):
+#     turtle.forward(100)
+#     turtle.right(36)
+# turtle.exitonclick()
+
+# for i in range(0,10):
+#     turtle.right(36)
+#     for i in range(0,5):
+#         turtle.forward(100)
+#         turtle.right(72)
+# turtle.exitonclick()
+
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90) 
+
+# for i in range(0,3):
+#     turtle.forward(100)
+#     turtle.left(120)
+
+# for i in range(0, 360):
+#     turtle.forward(1)
+#     turtle.right(1)
+
+# turtle.color("black", "yellow")
+# turtle.begin_fill()
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+
+# turtle.pendown()
+# turtle.color("black", "green")
+# turtle.begin_fill()
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+
+# turtle.pendown()
+# turtle.color("black", "blue")
+# turtle.begin_fill()
+# for i in range(0,4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.penup()
+# turtle.end_fill()
+# turtle.forward(100)
+
+# turtle.exitonclick()
+
+# for i in range(0,5):
+#     turtle.forward(100)
+#     turtle.right(144)
+# turtle.exitonclick()
+
+
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.penup()
+
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.pendown()
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.penup()
+
+# turtle.forward(100)
+# turtle.pendown()
+
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.penup()
+# turtle.right(180)
+# turtle.pendown()
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(50)
+# turtle.left(90)
+# turtle.forward(100)
+
+# turtle.exitonclick()
+
+# for i in range(0,8):
+#     turtle.color(random.choice(["black", "red", "yellow", "green", "blue", "pink"]))
+#     turtle.forward(50)
+#     turtle.right(45)
+
+# turtle.exitonclick()
+
+# for i in range(0,10):
+#     for j in range(0,8):
+#         turtle.forward(50)
+#         turtle.right(45)
+#     turtle.right(36)
+# turtle.hideturtle()
+
+# turtle.exitonclick()
+
+# line = random.randint(5,50)
+
+# for i in range(0, line):
+#     length = random.randint(25,100)
+#     angle = random.randint(1, 365)
+#     turtle.forward(length)
+#     turtle.right(angle)
+
+# turtle.exitonclick()
+
+
+
+
